c845b7e58a71764de1a8bc2ecfe271f809a7c096
rohan <rohanupreti4@gmail.com>
2026-01-01 13:49:24 +0545

    Clean GitHub repository

---
+++ MauiApp2/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+using MauiApp2.Services;
+using MudBlazor.Services;
+
+namespace MauiApp2
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+            builder.Services.AddMudServices();
+
+#if DEBUG
+    		builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+            builder.Services.AddSingleton<JournalState>();
+            return builder.Build();
+        }
+    }
+}
